6697f3e11d047cca8396b8f93c7f24053241b1c3
Jordan Ballard <jordanballard@Jordans-MacBook-Pro.local>
2023-06-08 11:20:50 -0700

    Use Angular and Bootstrap to build an SPA that makes API requests

--- app_admin/src/app/edit-trip/edit-trip.component.ts
+++ app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -46,7 +46,7 @@ export class EditTripComponent implements OnInit {
     this.tripService.getTrip(tripCode)
       .then(data => {
         console.log(data);
-        // Don't use editForm.setValue() as it will throw console error
+        // Don't use editForm.setValue(), it will throw console error
         this.editForm.patchValue(data[0]);
       })
   }
